65e7d3015af66f97a00db80a4aa47d4b054f82ff
John McLear <john@mclear.co.uk>
2013-11-17 23:37:31 +0000

    minor styling

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,8 @@
 exports.documentReady = function(){
   $.getScript("/static/plugins/ep_file_menu_toolbar/static/js/lib/jquery-css-dropdown-plugin-master/dropdown-menu.min.js", function(){
-    $('.dropdown-menu').dropdown_menu();
+    $('.dropdown-menu').dropdown_menu({
+      open_delay           : 50,                             // Delay on menu open
+      close_delay          : 300,                             // Delay on menu close
+    });
   });
 }
