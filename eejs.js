2f18f7921c82fcc60d52d7ae649a22756705f29e
John McLear <john@mclear.co.uk>
2020-11-22 16:27:25 +0000

    autofixes from Etherpad checkPlugins.js

--- eejs.js
+++ eejs.js
@@ -1,13 +1,10 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
+const eejs = require('ep_etherpad-lite/node/eejs/');
 
-exports.eejsBlock_styles = function(hook_name, args, cb){
-  args.content = args.content + "<style type='text/css'>"+eejs.require("ep_aa_file_menu_toolbar/static/js/lib/jquery-css-dropdown-plugin-master/dropdown-menu.css", {}, module) + "</style>";
+exports.eejsBlock_styles = function (hook_name, args, cb) {
+  args.content = `${args.content}<style type='text/css'>${eejs.require('ep_aa_file_menu_toolbar/static/js/lib/jquery-css-dropdown-plugin-master/dropdown-menu.css', {}, module)}</style>`;
   return cb();
-}
+};
 
-exports.eejsBlock_body = function (hook_name, args, cb)
-{
-  args.content = eejs.require('ep_aa_file_menu_toolbar/templates/toolbar.ejs', {settings : false}) + args.content;
-}
-
-
+exports.eejsBlock_body = function (hook_name, args, cb) {
+  args.content = eejs.require('ep_aa_file_menu_toolbar/templates/toolbar.ejs', {settings: false}) + args.content;
+};
--- static/js/index.js
+++ static/js/index.js
@@ -1,62 +1,60 @@
-exports.documentReady = function(){
-  $.getScript("../static/plugins/ep_aa_file_menu_toolbar/static/js/lib/jquery-css-dropdown-plugin-master/dropdown-menu.js", function(){
-    $(function() {
+exports.documentReady = function () {
+  $.getScript('../static/plugins/ep_aa_file_menu_toolbar/static/js/lib/jquery-css-dropdown-plugin-master/dropdown-menu.js', () => {
+    $(() => {
       $('.dropdown-menu').dropdown_menu({
-        open_delay  : 50, // Delay on menu open
-        close_delay : 300, // Delay on menu close
+        open_delay: 50, // Delay on menu open
+        close_delay: 300, // Delay on menu close
       });
     });
   });
-  $('#newPad').attr("href", "/p/"+randomPadName());
+  $('#newPad').attr('href', `/p/${randomPadName()}`);
 
-  var exports = $('#exportColumn').children("a");
-  $.each(exports, function(k,v){
-    var href = $(v).attr('href');
-    var txt = $(v).text();
-    var html = "<a class='exportlink' href='"+href+"'><div class='exportttype'>"+txt+"</div></a>";
+  const exports = $('#exportColumn').children('a');
+  $.each(exports, (k, v) => {
+    const href = $(v).attr('href');
+    const txt = $(v).text();
+    const html = `<a class='exportlink' href='${href}'><div class='exportttype'>${txt}</div></a>`;
     $('#file_menu_exports > .submenu').append(html);
   });
-  $("#importexportlink").click(function(){
+  $('#importexportlink').click(() => {
     $('#importfileinput').click();
-    $('#import_export').addClass("popup-show");
+    $('#import_export').addClass('popup-show');
   });
 
-  $("#filemenutoolbarsettings").click(function(){
-    $('#settings').addClass("popup-show");
+  $('#filemenutoolbarsettings').click(() => {
+    $('#settings').addClass('popup-show');
   });
 
-  $("#filemenutoolbarembed").click(function(){
-    $('#embed').addClass("popup-show");
+  $('#filemenutoolbarembed').click(() => {
+    $('#embed').addClass('popup-show');
   });
 
-  $("body").on("click", ".dropdown-menu-sub-indicator *[data-key='undo']", function(){
-    $("#editbar").find("*[data-key='undo']").click();
+  $('body').on('click', ".dropdown-menu-sub-indicator *[data-key='undo']", () => {
+    $('#editbar').find("*[data-key='undo']").click();
   });
 
-  $("body").on("click", ".dropdown-menu-sub-indicator *[data-key='redo']", function(){
-    $("#editbar").find("*[data-key='red']").click();
+  $('body').on('click', ".dropdown-menu-sub-indicator *[data-key='redo']", () => {
+    $('#editbar').find("*[data-key='red']").click();
   });
-}
+};
 
-function printPad(){
-
+function printPad() {
   window.print();
 
   // In chrome you have to fire the window.location.reload event to fire a print event.. More evidence Google = Microsoft
   // http://stackoverflow.com/questions/18622626/chrome-window-print-print-dialogue-opens-only-after-page-reload-javascript
 
-  if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1){
+  if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
     window.location.reload(); // Chrome is really broken here..
   }
-
 }
 
-function randomPadName(){
-  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  var string_length = 10;
-  var randomstring = '';
-  for (var i = 0; i < string_length; i++){
-    var rnum = Math.floor(Math.random() * chars.length);
+function randomPadName() {
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const string_length = 10;
+  let randomstring = '';
+  for (let i = 0; i < string_length; i++) {
+    const rnum = Math.floor(Math.random() * chars.length);
     randomstring += chars.substring(rnum, rnum + 1);
   }
   return randomstring;
