17ab566468ea1ba3bfcaf663d7b5ce4dbe73aa61
John McLear <john@mclear.co.uk>
2021-01-20 10:05:10 +0000

    Update eejs.js

--- eejs.js
+++ eejs.js
@@ -2,9 +2,10 @@ const eejs = require('ep_etherpad-lite/node/eejs/');
 
 exports.eejsBlock_styles = function (hook_name, args, cb) {
   args.content = `${args.content}<style type='text/css'>${eejs.require('ep_aa_file_menu_toolbar/static/js/lib/jquery-css-dropdown-plugin-master/dropdown-menu.css', {}, module)}</style>`;
-  return cb();
+  cb();
 };
 
 exports.eejsBlock_body = function (hook_name, args, cb) {
   args.content = eejs.require('ep_aa_file_menu_toolbar/templates/toolbar.ejs', {settings: false}) + args.content;
+  cb();
 };
